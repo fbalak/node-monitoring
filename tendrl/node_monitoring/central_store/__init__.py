5d93b2c8d527195ec1d188beb78f04fb346bb3a0
anmolbabu <anmolbudugutta@gmail.com>
2017-04-15 00:51:33 +0530

    Periodically update last_seen_at
    This is used by performance_monitoring to know the node status
    
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/node_monitoring/central_store/__init__.py
+++ tendrl/node_monitoring/central_store/__init__.py
@@ -1,4 +1,5 @@
 from tendrl.commons import central_store
+from tendrl.commons.utils.time_utils import now as tendrl_now
 
 
 class NodeMonitoringEtcdCentralStore(central_store.EtcdCentralStore):
@@ -10,3 +11,9 @@ class NodeMonitoringEtcdCentralStore(central_store.EtcdCentralStore):
 
     def save_definition(self, definition):
         NS.etcd_orm.save(definition)
+
+    def update_last_seen_at(self):
+        NS.etcd_orm.client.write(
+            '/monitoring/nodes/%s/last_seen_at' % NS.node_context.node_id,
+            tendrl_now().isoformat()
+        )
