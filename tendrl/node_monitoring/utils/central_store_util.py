69010e47b3e6211f9e80fa2238d6ee9acfc05ec6
anmolbabu <anmolbudugutta@gmail.com>
2017-06-23 12:47:49 +0530

    Fix etcd connection error once a etcd down is back up
    tendrl-bug-id: Tendrl/node-monitoring#70
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/node_monitoring/utils/central_store_util.py
+++ tendrl/node_monitoring/utils/central_store_util.py
@@ -1,8 +1,16 @@
 from tendrl.commons.utils.time_utils import now as tendrl_now
 
 
+def write_etcd(key, value):
+    try:
+        NS._int.wclient.write(key, value, quorum=True)
+    except (AttributeError, EtcdException) as ex:
+        NS._int.wreconnect()
+        NS._int.wclient.write(key, value, quorum=True)
+
+
 def update_last_seen_at():
-    NS._int.wclient.write(
+    write_etcd(
         '/monitoring/nodes/%s/last_seen_at' % NS.node_context.node_id,
         tendrl_now().isoformat()
     )
