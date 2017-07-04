4a1a99736292c5ff4a20d60e934865c45f839706
anmolbabu <anmolbudugutta@gmail.com>
2017-07-04 09:16:23 +0530

    Update central_store_util.py

--- tendrl/node_monitoring/utils/central_store_util.py
+++ tendrl/node_monitoring/utils/central_store_util.py
@@ -3,7 +3,7 @@ import tendrl.commons.utils.etcd_utils as etcd_utils
 
 
 def update_last_seen_at():
-    etcd_utils.write_etcd(
+    etcd_utils.write(
         '/monitoring/nodes/%s/last_seen_at' % NS.node_context.node_id,
         tendrl_now().isoformat()
     )
